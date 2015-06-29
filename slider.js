8338742625f0c342f3c5712f3a85efd410058a73
postolenko <denis.postolenko@gmail.com>
2015-06-29 22:18:53 +0300

    Slider Module Denis Postolenko

--- slider.js
+++ slider.js
@@ -181,7 +181,7 @@ $(document).ready(function() {
 
 		// if($(window).width() > 480) {
 
-			// clearTimeout(sliderStart);
+			clearTimeout(sliderStart);
 
 			sliderStart = setTimeout(function() {
 
